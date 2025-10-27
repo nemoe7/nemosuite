df653374bcda3a23147ea5c463932d326747fb27
nemo / stan <stanpai78@gmail.com>
2025-10-27 15:27:20 +0800

    refactor: Introduce constants for reach distances

--- fabric/src/main/java/com/nemo/suite/util/fabric/ReachHelperImpl.java
+++ fabric/src/main/java/com/nemo/suite/util/fabric/ReachHelperImpl.java
@@ -3,11 +3,13 @@ package com.nemo.suite.util.fabric;
 import net.minecraft.client.player.LocalPlayer;
 
 public class ReachHelperImpl {
+  private static final double DEFAULT_ENTITY_REACH = 3.0D;
+  private static final double DEFAULT_BLOCK_REACH = 4.5D;
   public static double getEntityReach(LocalPlayer player) {
-    return player.isCreative() ? 6.0D : 3.0D;
+    return player.isCreative() ? DEFAULT_ENTITY_REACH + 2.0D : DEFAULT_ENTITY_REACH;
   }
 
   public static double getBlockReach(LocalPlayer player) {
-    return player.isCreative() ? 5.0D : 4.5D;
+    return player.isCreative() ? DEFAULT_BLOCK_REACH + 0.5D : DEFAULT_BLOCK_REACH;
   }
 }
