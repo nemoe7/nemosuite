09453a4bbc9fbd2d6e6a5eb90761aea4ada1653e
nemo / stan <stanpai78@gmail.com>
2025-10-27 17:53:15 +0800

    feat: Add creative mode reach bonus to forge ReachHelper

--- forge/src/main/java/com/nemo/suite/util/forge/ReachHelperImpl.java
+++ forge/src/main/java/com/nemo/suite/util/forge/ReachHelperImpl.java
@@ -5,10 +5,10 @@ import net.minecraftforge.common.ForgeMod;
 
 public class ReachHelperImpl {
   public static double getEntityReach(LocalPlayer player) {
-    return player.getAttributeValue(ForgeMod.ATTACK_RANGE.get());
+    return player.getAttributeValue(ForgeMod.ATTACK_RANGE.get()) + (player.isCreative() ? 3.0D : 0.0D);
   }
 
   public static double getBlockReach(LocalPlayer player) {
-    return player.getAttributeValue(ForgeMod.REACH_DISTANCE.get());
+    return player.getAttributeValue(ForgeMod.REACH_DISTANCE.get()) + (player.isCreative() ? 0.5D : 0.0D);
   }
 }
